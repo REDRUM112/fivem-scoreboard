65239a7c750455961c91fcc7af19cc0243c2f4d6
REDRUM112 <zerkyoags@gmail.com>
2020-04-12 06:14:02 -0700

    Update connection.php

--- include/connection.php
+++ include/connection.php
@@ -1,5 +1,5 @@
 <?php
-include 'C:\dummy\inventory\fmsb\example-esx\include\config.php';
+include 'config.php';
 
  $link = mysqli_connect($host, $user, $password, $db);
 if (mysqli_connect_errno()) {
